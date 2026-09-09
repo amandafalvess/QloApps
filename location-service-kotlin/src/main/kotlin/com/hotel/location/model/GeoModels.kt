c4e3131fadc4ff0f9509479f8f05bf6b60c72b40
amandafalvess <amandafalves@google.com>
2026-09-09 22:25:40 +0000

    refactor(location-service-kotlin): extrair enums de geofence para arquivo dedicado GeoEnums.kt
    Move os enums GeofenceState e GeofenceTransition de GeoModels.kt para GeoEnums.kt, melhorando a modularizacao e atendendo a revisao de codigo da RFC-004.

--- location-service-kotlin/src/main/kotlin/com/hotel/location/model/GeoModels.kt
+++ location-service-kotlin/src/main/kotlin/com/hotel/location/model/GeoModels.kt
@@ -2,26 +2,7 @@ package com.hotel.location.model
 
 import com.hotel.location.exception.InvalidCoordinatesException
 import com.hotel.location.exception.InvalidGeofenceRadiusException
-import com.hotel.location.exception.InvalidGeofenceStateException
 import com.hotel.location.exception.MissingFieldException
-
-enum class GeofenceState {
-    INSIDE,
-    OUTSIDE;
-
-    companion object {
-        fun fromString(value: String): GeofenceState {
-            return entries.firstOrNull { it.name.equals(value.trim(), ignoreCase = true) }
-                ?: throw InvalidGeofenceStateException(value)
-        }
-    }
-}
-
-enum class GeofenceTransition {
-    ENTERED,
-    EXITED,
-    NO_CHANGE
-}
 
 data class Coordinates(
     val latitude: Double,
